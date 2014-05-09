acb5fb9b4b46d3440fde7cffa6d67c3fd2a604cc
Alex Brazier <aejbrazier@gmail.com>
2014-05-09 03:48:22 +0100

    Get coursework mark in getModules coursework

--- ness.js
+++ ness.js
@@ -58,6 +58,12 @@ exports.getModules = function(detail, user, callback)
 
                         if(courseworkLink.attr('title') !== undefined)
                             coursework.due = moment(courseworkLink.attr('title'), 'HH:mm:ss , D MMM YYYY').format()
+                        var courseworkMark = $(tds[1]).find('b').first();
+                        if(courseworkMark != '')
+                        coursework.mark = {
+                                mark: courseworkMark.find('span').text(),
+                                total: courseworkMark.text().match(/\d+$/)[0]
+                        };
 
                         module.coursework.push(coursework);
                     });
