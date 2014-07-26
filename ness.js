b5402258089a2c9a4132f6282319c4466901c70e
Alex Brazier <aejbrazier@gmail.com>
2014-07-26 22:34:16 +0100

    Initial fix #1 - currently only works for single user

--- ness.js
+++ ness.js
@@ -4,6 +4,7 @@ var request = require('request'),
     moment = require('moment'),
     errors = require('./errors'),
     _ = require('underscore');
+var request = request.defaults({jar: true});
 
 var name;
 
@@ -506,6 +507,39 @@ exports.getSpec = function(exid, user, callback)
         });
 }
 
+exports.login = function(user, callback)
+{
+    var url = "https://ness.ncl.ac.uk";
+    request.get(url, function(e, r, body) {
+        request.post('https://gateway.ncl.ac.uk/idp/Authn/UserPassword', {form:{j_username: user.id, j_password: user.pass, _eventId: 'submit', submit: 'LOGIN'}}, function (e, r, body) {
+            request.get(url, function (error, response, body) {
+                var $ = cheerio.load(body);
+                var $form = $('form');
+                var action = $form.attr('action');
+                var response = $form.find('input[name=SAMLResponse]').attr('value');
+                request.post(action, {form:{SAMLResponse: response}}, function (e, r, body) {
+                    var cookie = r.headers["set-cookie"][0];
+                    request.get(url, function (error, response, body) {
+                        if (!e)
+                        {
+                            var $ = cheerio.load(body);
+                            var response = {
+                                name: $('#uname').text().trim().split(' (')[0],
+                                cookie: cookie
+                            }
+                            callback(null, response);
+                        }
+                        else
+                        {
+                            callback(e || {error: 401}, null);
+                        }
+                    });
+                });
+            });
+        });
+    });
+}
+
 function getPage(user, url, callback)
 {
     if(user.dev) {
@@ -516,25 +550,29 @@ function getPage(user, url, callback)
         callback(null, $);
     }
     else {
-        request({
-          uri: url,
-          auth: {
-            user: user.id, pass: user.pass,
-            sendImmediately: false
-          }
-        }, function (error, response, body)
-        {
-            if (!error && response.statusCode == 200)
+        if(!user.cookie)
+            callback({error: 401}, null);
+        else {
+            var headers = {
+                'Cookie': user.cookie
+            };
+            request.get({
+              url: url,
+              headers: headers
+            }, function (error, response, body)
             {
-                var $ = cheerio.load(body);
+                if (!error && response.statusCode == 200)
+                {
+                    var $ = cheerio.load(body);
 
-                callback(null, $);
-            }
-            else
-            {
-                callback(error || {error: 401}, null);
-            }
-        });
+                    callback(null, $);
+                }
+                else
+                {
+                    callback(error || {error: 401}, null);
+                }
+            });
+        }
     }
 }
 
